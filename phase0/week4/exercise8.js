3ed7be854d4e26df3b0b8b67a5345d39e0ebd2ad
igun369 <guntoro.gyk@gmail.com>
2019-07-16 22:33:26 +0700

    edite exercise8.js

--- phase0/week4/exercise8.js
+++ phase0/week4/exercise8.js
@@ -21,6 +21,4 @@ console.log(tukarBesarKecil('Hello World')); // "hELLO wORLD"
 console.log(tukarBesarKecil('I aM aLAY')); // "i Am Alay"
 console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
 console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
-  console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
-
-console.log()
+console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
