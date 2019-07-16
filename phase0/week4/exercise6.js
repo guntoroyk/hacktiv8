3c7f8e97b2efbdaa6bdb54b94d1e45c3870d6843
igun369 <guntoro.gyk@gmail.com>
2019-07-16 20:40:56 +0700

    exercise6 finished

--- phase0/week4/exercise6.js
+++ phase0/week4/exercise6.js
@@ -1,28 +1,52 @@
 function digitPerkalianMinimum(angka) {
-    // you can only write your code here!
-    let faktor = [];
-    for (let i = 1; i <= angka; i++) {
-        if (angka % i === 0) {
-            if (i !== faktor[1]) {
-                faktor.push([i, angka/i]);
-            }
-        }
+  // you can only write your code here!
+  let faktor = [];
+  let terpendek = 0;
+  
+  for (let i = 1; i <= angka; i++) {
+    if (angka % i === 0) {
+      if (i !== faktor[1]) {
+        faktor.push([i, angka/i]);
+      }
     }
-
-    return faktor;
   }
   
-  // TEST CASES
-//   console.log(digitPerkalianMinimum(24)); // 2
-  console.log(digitPerkalianMinimum(90)); // 3
-//   console.log(digitPerkalianMinimum(20)); // 2
-//   console.log(digitPerkalianMinimum(179)); // 4
-//   console.log(digitPerkalianMinimum(1)); // 2
+  for (let i = 0; i < faktor.length; i++) {
+    let faktorDigabung = myJoin(faktor[i]);
+    
+    if (i === 0) {
+      terpendek = faktorDigabung.length;
+    } else {
+      if (faktorDigabung.length < terpendek) {
+        terpendek = faktorDigabung.length;
+      }
+    }
+  }
+  return terpendek;
+}
 
-  /**
-   * 20
-   * 1 20
-   * 2 10
-   * 4 5
-   * 
-   */
+function myJoin(input) {
+  let result = "";
+  
+  for (let i = 0; i < input.length; i++) {
+    result += input[i];
+  }
+  return result;
+}
+
+
+// console.log(myJoin([1,3,2]));
+// TEST CASES
+console.log(digitPerkalianMinimum(24)); // 2
+console.log(digitPerkalianMinimum(90)); // 3
+console.log(digitPerkalianMinimum(20)); // 2
+console.log(digitPerkalianMinimum(179)); // 4
+console.log(digitPerkalianMinimum(1)); // 2
+
+/**
+* 20
+* 1 20
+* 2 10
+* 4 5
+* 
+*/
